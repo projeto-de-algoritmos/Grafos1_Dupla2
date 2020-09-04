58e0de2c9225fd16b9c194da011f10fcf885b6bb
Paulo Victor <paulovmel@gmail.com>
2020-09-04 10:27:16 -0300

    create: Homepage component

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import { Container } from './styles';
+
+const Homepage: React.FC = () => {
+  
+  return (
+    <Container>
+      <h1>Hello World</h1>
+    </Container>
+  );
+};
+
+export default Homepage;
