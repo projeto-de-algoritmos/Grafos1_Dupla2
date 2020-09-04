b87b5bda7dac863dc0446179302c2d99e29b8492
Paulo Victor <paulovmel@gmail.com>
2020-09-04 12:38:01 -0300

    Create Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import { Container } from './styles';
+
+const Home = () => {
+
+
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default Home;
